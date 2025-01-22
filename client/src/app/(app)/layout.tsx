42904b769e5ae2b7132035d6d0e6e1f49a13f652
Youngchul Lim <ciyciyjjoni@naver.com>
2025-01-23 02:30:02 +0900

    Ing - ErrorBoundary for Testing

--- client/src/app/(app)/layout.tsx
+++ client/src/app/(app)/layout.tsx
@@ -3,7 +3,9 @@
 import ClientQueryProvider from "@/providers/ClientQueryProvider";
 import { useAccountStore } from "@/stores";
 import { useEffect } from "react";
-
+import { QueryErrorResetBoundary } from "@tanstack/react-query";
+import { ErrorBoundary } from 'react-error-boundary';
+import { Button } from "@/components/ui/button";
 
 export interface AppLayoutProps {
     children: React.ReactNode
@@ -14,9 +16,23 @@ const AppLayout = ({children}:AppLayoutProps) => {
 
     return (
         <ClientQueryProvider>
-        <div className="flex flex-1 flex-col">
-            <main className="flex flex-1 flex-col">{children}</main>
-        </div>
+            <QueryErrorResetBoundary>
+                {({ reset }) => (
+                    <ErrorBoundary
+                        onReset={reset}
+                        fallbackRender={({ error, resetErrorBoundary }) => (
+                            <div role="alert">
+                              There was an error! { error?.message }
+                              <Button onClick={() => resetErrorBoundary()}>Try again</Button>
+                            </div>
+                          )}
+                    >
+                    <div className="flex flex-1 flex-col">
+                        <main className="flex flex-1 flex-col">{children}</main>
+                    </div>
+                    </ErrorBoundary>
+                )}
+            </QueryErrorResetBoundary>
         </ClientQueryProvider>
     );
 }
